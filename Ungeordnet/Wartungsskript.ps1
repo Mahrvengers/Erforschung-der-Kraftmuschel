b09d451d05fe71b305be99edb65cb82cfe64e52c
Stefan Hoffmann <Stefan.Hoffmann@mahr-edv.de>
2018-04-29 11:59:31 +0200

    + Dienste abrufen im Wartungsskript

--- Ungeordnet/Wartungsskript.ps1
+++ Ungeordnet/Wartungsskript.ps1
@@ -12,4 +12,16 @@
     4. Backups checken
     5. Netzwerkinformationen sammeln (WLAN usw.)
     6. Status Updates + Ausführung Updates
+    7. Remote Prüfung aller vorgegangenen Ziele 
+    8. einfache visuelle Oberfläche für alles
+    9. Dienste White/Blacklisten für Prüfungen
 #>
+
+# Dienste abrufen
+
+Get-Service | Where-Object {
+    $_.StartType -EQ "Automatic" -and $_.Status -NE "Running"
+}  | Out-GridView -Title "Dienste, die automatisch gestartet werden sollten, aber nicht laufen?" -Wait
+
+
+# 
