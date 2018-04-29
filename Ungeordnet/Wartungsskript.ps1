2a64362190c1b161087044749800308cf538caa6
Stefan Hoffmann <Stefan.Hoffmann@mahr-edv.de>
2018-04-29 11:03:09 +0200

    Ziele definiert

---
+++ Ungeordnet/Wartungsskript.ps1
@@ -0,0 +1,15 @@
+<#
+.SYNOPSIS
+    Unser Wartungsskript
+#>
+
+
+<# 
+    Ziele:
+    1. Dienste abrufen 
+    2. Ereignisse prüfen
+    3. Ereignislogs rotieren
+    4. Backups checken
+    5. Netzwerkinformationen sammeln (WLAN usw.)
+    6. Status Updates + Ausführung Updates
+#>
