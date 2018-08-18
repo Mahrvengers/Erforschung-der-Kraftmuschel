182afcee515c946f12d918161939810713f30315
Stefan Hoffmann <stho32github@gmail.com>
2018-08-18 16:45:38 +0200

    + Day7

---
+++ Learn-Windows-Powershell-in-a-month-of-lunches/Day7.ps1
@@ -0,0 +1,22 @@
+<#
+
+    Day 7 : Adding commands
+
+#>
+
+<#
+
+    For this lab, you have only one task: Run the Networking troubleshooting pack.
+    When you successfully do so, you'll be asked for an Instance ID. Hit Enter, run
+    a WebConnectivityCheck, and ask for help connection to a specific web page. 
+    Use www.pluralsight.com/browse/it-ops as your test URL. We hope you get a 
+    "No problems were detected" report, meaning you ran the check successfully.
+
+    To accomplish this task, you need to discover a command capable of getting a troubleshooting
+    pack, and another capable of executing a troubleshooting pack. You also need
+    to discover where the packs are located and how they're named. Everything
+    you need to know is in Powershell, and the help system will find it for you.
+
+    That's all the help you will get!
+
+#>
